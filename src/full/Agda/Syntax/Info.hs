d51b7f3e3e32fc5361e3ea57a8934956230b40ea
Andreas Abel <andreas.abel@ifi.lmu.de>
2020-06-21 15:57:54 +0200

    [ fixed #4769 ] warn when EqualP comes with Name or ArgInfo

--- src/full/Agda/Syntax/Info.hs
+++ src/full/Agda/Syntax/Info.hs
@@ -240,7 +240,7 @@ instance Null LHSInfo where
 -- | For a general pattern we remember the source code position.
 newtype PatInfo
   = PatRange Range
-  deriving (Data, Eq, Null, Show, SetRange, HasRange, KillRange)
+  deriving (Data, Eq, Null, Semigroup, Monoid, Show, SetRange, HasRange, KillRange)
 
 -- | Empty range for patterns.
 patNoRange :: PatInfo
